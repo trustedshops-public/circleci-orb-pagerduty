b313f5aa575308ea51c75f599578c0fda75b3bef
Timo Reymann <Timo.Reymann@trustedshops.de>
2022-02-09 08:51:26 +0100

    feat: Add initial orb sources (#1)
    * feat: Add initial commands for maintenance windows
    
    * fix: Fix duration type
    
    * fix: Fix parameters for create maintenance window
    
    * fix: Fix fail arg for curl
    
    * fix: Evaulate description for maintenance
    
    * feat: Evaluate end maintenance window id
    
    * feat: Add id output for maintenance window
    
    * fix: Fix maintenance window default id
    
    * fix: Fix duplicate eval
    
    * feat: Add run-with-maintenance command
    
    * docs: Add examples, add jq requirement to orb description

---
+++ src/scripts/maintenance-window-create.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq is required, but could not be found."
+    exit 2
+fi
+
+# Join all arguments by given separator
+function join_by {
+    local IFS="$1";
+    shift;
+    echo "$*";
+}
+
+# Escape string for json
+function json_escape() {
+    jq -R -s '.' <<< "$1"
+}
+
+PARAM_DESCRIPTION=$(eval echo "$PARAM_DESCRIPTION")
+
+# date format to use for pagerduty
+date_format='%Y-%m-%dT%H:%M:%S-00:00'
+pagerduty_token=$(eval echo "\$$PARAM_PAGERDUTY_TOKEN")
+
+# Build service list with json objects
+IFS=","
+services_json=()
+for service in ${PARAM_SERVICES}
+do
+    services_json+=('{"id": "'"${service}"'", "type": "service_reference"}')
+done
+
+# Create maintenance
+response=$(curl --request POST \
+  --fail \
+  --url "https://api.pagerduty.com/maintenance_windows" \
+  --header 'Accept: application/vnd.pagerduty+json;version=2' \
+  --header "Authorization: Token token=${pagerduty_token}" \
+  --header 'Content-Type: application/json' \
+  --header "From: ${PARAM_PAGERDUTY_MAIL}" \
+  --data @- <<TEXT
+    {
+        "maintenance_window": {
+            "type": "maintenance_window",
+            "start_time": "$(date -u +${date_format})",
+            "end_time": "$(date -u -d "+ ${PARAM_DURATION_MINUTES} minutes" +${date_format})",
+            "description": $(json_escape "${PARAM_DESCRIPTION}"),
+            "services": [$(join_by "," "${services_json[@]}")]
+        }
+    }
+TEXT
+)
+
+# Extract and set env var
+id=$(echo "$response" | jq -r ".maintenance_window.id")
+export PAGERDUTY_MAINTENANCE_WINDOW_ID="${id}"
+echo "export PAGERDUTY_MAINTENANCE_WINDOW_ID='${id}'" >> "$BASH_ENV"
+
+echo "Maintenance window created: ${PAGERDUTY_MAINTENANCE_WINDOW_ID}"
---
+++ src/scripts/maintenance-window-delete.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq is required, but could not be found."
+    exit 2
+fi
+
+pagerduty_token=$(eval echo "\$$PARAM_PAGERDUTY_TOKEN")
+PARAM_MAINTENANCE_WINDOW_ID=$(eval echo "\$$PARAM_MAINTENANCE_WINDOW_ID")
+
+curl --request DELETE \
+  --url "https://api.pagerduty.com/maintenance_windows/${PARAM_MAINTENANCE_WINDOW_ID}" \
+  --header 'Accept: application/vnd.pagerduty+json;version=2' \
+  --header "Authorization: Token token=${pagerduty_token}" \
+  --header 'Content-Type: application/json' | jq
