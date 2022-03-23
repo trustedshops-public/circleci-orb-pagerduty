cdc60105192097a0a5e0b20f29b736536342c0de
Timo Reymann <Timo.Reymann@trustedshops.de>
2022-03-23 09:14:11 +0100

    feat: End maintenance window in all cases (#2)
    * feat: End maintenance window in all cases
    
    * docs: Update description for delete-maintenance-window command
    
    * fix: Fix orb syntax for run

--- src/scripts/maintenance-window-delete.sh
+++ src/scripts/maintenance-window-delete.sh
@@ -9,8 +9,14 @@ fi
 pagerduty_token=$(eval echo "\$$PARAM_PAGERDUTY_TOKEN")
 PARAM_MAINTENANCE_WINDOW_ID=$(eval echo "\$$PARAM_MAINTENANCE_WINDOW_ID")
 
-curl --request DELETE \
-  --url "https://api.pagerduty.com/maintenance_windows/${PARAM_MAINTENANCE_WINDOW_ID}" \
-  --header 'Accept: application/vnd.pagerduty+json;version=2' \
-  --header "Authorization: Token token=${pagerduty_token}" \
-  --header 'Content-Type: application/json' | jq
+if [ -z "${PARAM_MAINTENANCE_WINDOW_ID}" ]
+then
+  echo "WARNING: Maintenance window does not exist. Ignoring, if the maintenance window creation did not fail, make sure to check your pipeline is built as expected!"
+else
+  echo "Maintenance window found, ending it."
+  curl --request DELETE \
+    --url "https://api.pagerduty.com/maintenance_windows/${PARAM_MAINTENANCE_WINDOW_ID}" \
+    --header 'Accept: application/vnd.pagerduty+json;version=2' \
+    --header "Authorization: Token token=${pagerduty_token}" \
+    --header 'Content-Type: application/json' | jq
+fi
